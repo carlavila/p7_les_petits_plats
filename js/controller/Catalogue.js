781da2c6c7510952b8d0fd2a783a1e7057362d14
carla <76110813+carlavila@users.noreply.github.com>
2023-01-12 19:15:53 +0100

    algo 2

--- js/controller/Catalogue.js
+++ js/controller/Catalogue.js
@@ -145,16 +145,24 @@ class Catalogue {
       
 	/* Filter with searchBar */
 	filter = (query) => {
-	  this.catalogueFiltred = this.catalogue.filter((recipe) => {
-	    return (
-	      recipe.name.toLowerCase().includes(query) ||
-	      recipe.description.toLowerCase().includes(query) ||
-	      recipe.ingredients.some((ingredient) =>
-		ingredient.ingredient.toLowerCase().includes(query)
-	      )
-	    );
-	  });
-	};
+		this.catalogueFiltred = []
+		for(let recipe of this.catalogue) {
+		  const recipeIngredientsList = new Array()
+		  for (let ingredient of recipe.ingredients) {
+		    recipeIngredientsList.push(...ingredient.ingredient.toLowerCase().split(' '))
+		  }
+	    
+		  if(
+		    recipe.name.toLowerCase().includes(query) || 
+		    recipe.description.toLowerCase().includes(query) ||
+		    recipeIngredientsList.includes(query)) {
+		    this.catalogueFiltred.push(recipe)
+		  }
+		}
+	      };
+	      
+	
+
       
 	/* Filter with tags */
 	tag = () => {
