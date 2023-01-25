e4f77284269d4ab88b85b37c3d42c7954be94534
carla <76110813+carlavila@users.noreply.github.com>
2023-01-25 13:31:25 +0100

    finalisation

--- js/templates/Recipe.js
+++ js/templates/Recipe.js
@@ -1,15 +1,17 @@
 class Recipe {
-	constructor(data) {
-	  this.id = data.id;
-	  this.name = data.name;
-	  this.time = data.time;
-	  this.ingredients = data.ingredients;
-	  this.description = data.description;
-	  this.cardWrapper = document.getElementById("recipes")
-	}
-      
-	firstBuilder = () => {
-	  const html = `  
+  //Parent
+  constructor(data) {
+    //Argument
+    this.id = data.id; //Le mot clé this commence à faire référence au nouvel objet et il devient l'objet d'instance actuel. Le nouvel objet est ensuite renvoyé comme valeur de retour du constructeur.
+    this.name = data.name;
+    this.time = data.time;
+    this.ingredients = data.ingredients;
+    this.description = data.description;
+    this.cardWrapper = document.getElementById("recipes");
+  }
+
+  firstBuilder = () => {
+    const html = `  
 	    <article class="card-container">
 		<div class="card_image-container">
 		</div>
@@ -28,38 +30,39 @@ class Recipe {
 		    </div>
 		    <div class="card_items-recipe">
 			<ul class="recipes_ingredients-list">
-	    `
-      
-	  return html;
-	}
-      
-	secondBuilder = () => {
-	  let html = ''
-      
-	  this.ingredients.forEach(obj => {
-	    
-	    html += '<li>'
-      
-	    if (obj.hasOwnProperty('ingredient')) {
-	      html += `<span class="composant">${obj.ingredient} : </span>`
-	    }
-      
-	    if (obj.hasOwnProperty('quantity')) {
-	      html += `<span class="composant_results">${obj.quantity} </span>`
-	    }
-      
-	    if (obj.hasOwnProperty('unit')) {
-	      html += `<span class="composant_results">${obj.unit} </span>`
-	    }
-      
-	    html += '</li>'
-	  })
-      
-	  return html
-	}
-      
-	thirdBuilder = () => {
-	  const html = `
+	    `;
+
+    return html;
+  };
+
+  secondBuilder = () => {
+    let html = "";
+
+    this.ingredients.forEach((obj) => {
+      //La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
+      html += "<li>";
+
+      if (obj.hasOwnProperty("ingredient")) {
+        //hasOwnProperty() retourne un booléen indiquant si l'objet possède la propriété spécifiée, sans que celle-ci provienne de la chaîne de prototypes de l'objet.
+        html += `<span class="composant">${obj.ingredient} : </span>`;
+      }
+
+      if (obj.hasOwnProperty("quantity")) {
+        html += `<span class="composant_results">${obj.quantity} </span>`;
+      }
+
+      if (obj.hasOwnProperty("unit")) {
+        html += `<span class="composant_results">${obj.unit} </span>`;
+      }
+
+      html += "</li>";
+    });
+
+    return html;
+  };
+
+  thirdBuilder = () => {
+    const html = `
 		    </ul>
 		    <div class="recipe_description">
 		      <p>
@@ -70,12 +73,13 @@ class Recipe {
 		</div>
 	      </article>
 	  `;
-	  return html;
-	};
-      
-	render = () => {
-	  const li = document.createElement("li");
-	  li.innerHTML = this.firstBuilder() + this.secondBuilder() + this.thirdBuilder();
-	  this.cardWrapper.appendChild(li);
-	};
-      }
+    return html;
+  };
+
+  render = () => {
+    const li = document.createElement("li");
+    li.innerHTML =
+      this.firstBuilder() + this.secondBuilder() + this.thirdBuilder();
+    this.cardWrapper.appendChild(li);
+  };
+}
--- js/templates/TagBtn.js
+++ js/templates/TagBtn.js
@@ -1,61 +1,55 @@
 class TagBtn {
-	constructor(name, type) {
-	  this.name = name;
-	  this.type = type;
-	}
-      
-	render() {
-	  /* HTML - TagCloseBtn */
-      
-	  const $wrapper = document.getElementById("tag-container");
-      
-	  const $tagWrapper = document.createElement("div");
-	  const $divWording = document.createElement("div");
-	  const $divCloseTag = document.createElement("div");
-      
-	  $tagWrapper.classList.add("tag", `tag-${this.type}`);
-	  $divWording.classList.add("tag-wordin");
-	  $divCloseTag.classList.add("tag-wording");
-      
-	  $divWording.innerHTML = `<span class="wording">${this.name}</span>`;
-	  $divCloseTag.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
-      
-	  $tagWrapper.appendChild($divWording);
-	  $tagWrapper.appendChild($divCloseTag);
-	  $wrapper.appendChild($tagWrapper);
-      
-	  /* EVENT - close TagBtn */
-      
-	  $divCloseTag.addEventListener("click", (e) => {
-	    switch (this.type) {
-	      case "ingredient":
-		catalogue.ingredientsTags.splice(
-		  catalogue.ingredientsTags.indexOf(this.name),
-		  1
-		);
-		break;
-      
-	      case "appliance":
-		catalogue.appliancesTags.splice(
-		  catalogue.appliancesTags.indexOf(this.name),
-		  1
-		);
-		break;
-      
-	      case "ustensil":
-		catalogue.ustensilsTags.splice(
-		  catalogue.ustensilsTags.indexOf(this.name),
-		  1
-		);
-		break;
-      
-	      default:
-		console.log("error : type not found");
-		break;
-	    } // end switch
-      
-	    e.target.parentElement.parentElement.remove();
-	    catalogue.tag();
-	  });
-	}
-      }
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  render() {
+    /* HTML - TagCloseBtn */
+    const $wrapper = document.getElementById("tag-container");
+    const $tagWrapper = document.createElement("div");
+    const $divWording = document.createElement("div");
+    const $divCloseTag = document.createElement("div");
+    $tagWrapper.classList.add("tag", `tag-${this.type}`);
+    $divWording.classList.add("tag-wordin");
+    $divCloseTag.classList.add("tag-wording");
+    $divWording.innerHTML = `<span class="wording">${this.name}</span>`;
+    $divCloseTag.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
+    $tagWrapper.appendChild($divWording);
+    $tagWrapper.appendChild($divCloseTag);
+    $wrapper.appendChild($tagWrapper);
+
+    /* EVENT - close TagBtn */
+    $divCloseTag.addEventListener("click", (e) => {
+      switch (this.type) {
+        case "ingredient":
+          catalogue.ingredientsTags.splice(
+            catalogue.ingredientsTags.indexOf(this.name),
+            1
+          );
+          break;
+
+        case "appliance":
+          catalogue.appliancesTags.splice(
+            catalogue.appliancesTags.indexOf(this.name),
+            1
+          );
+          break;
+
+        case "ustensil":
+          catalogue.ustensilsTags.splice(
+            catalogue.ustensilsTags.indexOf(this.name),
+            1
+          );
+          break;
+
+        default:
+          console.log("error : type not found");
+          break;
+      } // end switch
+
+      e.target.parentElement.parentElement.remove();
+      catalogue.tag();
+    });
+  }
+}
